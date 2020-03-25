500aa616594f54895f9b92db56038233cdbcf6af
unknown <wcganastacia@yandex.ru>
2020-03-25 21:23:24 +0300

    add treangle

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,16 +11,58 @@ function getValues (){
     var i=0;
 
     do{
-        result[i]= +prompt ('Введите число');
-        i++;
+        result[i++]= +prompt ('Введите число');
         var isProcess=confirm ('Продолжить');
     }while (isProcess);
+
     return result;
 }
 
+function calculator(){
 do{
     var numbers = getValues();
-    alert(numbers);
+    var res = numbers [0];
+
+   for(var i = 1; i < numbers.length; i++){
+       res = res + numbers [i];
+   }
+
+    alert('Итого' + res);
 
     var isProcess = confirm ('Продолжить');
 } while (isProcess);
+}
+
+// calculator
+
+function triangle(lines) {
+    var line = '*';
+
+    for(var i = 0; i < rows [0]; i++) {
+        console.log(line);
+        line += '*';
+    }
+}
+
+//var rows = getValues();
+
+//triangle(rows[5]); 
+
+function triangleReverse(lines, sign){
+    var strings = [];
+    var line;
+
+ for(var i = 0; i < lines; i++){
+    line = '';
+    for(var j = 0; j < lines - i; j++ ){
+        line += sign;
+    }
+    strings[i] = line;
+    console.log(strings[i]);
+ }
+
+    for ( var i = strings.length - 1; i >= 0; i--){
+        console.log(strings[i]);
+    }
+}
+triangleReverse(5, '*');
